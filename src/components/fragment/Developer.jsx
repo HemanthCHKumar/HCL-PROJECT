4aada12a2d12bfee0911222d0dbfe765c048428f
hemanth kumar <hemanthchintala519@gmail.com>
2023-05-27 17:50:13 +0530

    Initiasl

--- src/components/fragment/Developer.jsx
+++ src/components/fragment/Developer.jsx
@@ -10,31 +10,31 @@ const Developer = () => {
             <h3 className={"Developer-head"}>Meet the developer</h3>
             <div className="Developer-profile">
                 <div className="Developer-profileCard">
-                    <img src={AvatarImage} alt="Profile"/>
+                    {/* <img src={AvatarImage} alt="Profile"/> */}
                     <div className={"Card-details"}>
-                        <h3>Vishal Singh</h3>
+                        <h3>Chintala HEMANTH KUMAR</h3>
                         <p>Full Stack developer</p>
                         <p>Competitive Coder</p>
                     </div>
                 </div>
                 <div className="Developer-profileDetails">
-                    <p>A Computer Science and Engineering Student at Lovely Professional University, Punjab.</p>
+                    <p>A Computer Science and Engineering Studting at sathyabama unviersity , CHENNAI</p>
                     <p>Graduating in 2022 and looking for a responsible position to gain practical knowledge</p>
                     <p>A full-stack web developer and a Competitive coder.</p>
                     <p>I love designing fully responsive websites.</p>
                     <p>I have a keen interest in developing projects, whenever I want to learn something new.</p>
                     <p>And a blogger.</p>
                     <div className="Card-btn">
-                        <IconButton target={"_blank"}  href={"https://www.facebook.com/vishal.kld"} title={"vishal.kld"}>
+                        <IconButton target={"_blank"}  href={"https://www.facebook.com/chintala.hemanth.9"} title={"Hemanth"}>
                             <Facebook/>
                         </IconButton>
-                        <IconButton target={"_blank"} href={"https://twitter.com/Vishal_kld"}  title={"Vishal_kld"}>
+                        <IconButton target={"_blank"} href={"https://twitter.com/hemanth_781?t=5fWBqQFwuQPAMCfN--hQNw&s=08"}  title={"Hemanth"}>
                             <Twitter/>
                         </IconButton>
-                        <IconButton target={"_blank"} href={"https://www.linkedin.com/in/vishalsingh-/"}  title={"vishalsingh-"}>
+                        <IconButton target={"_blank"} href={"https://www.linkedin.com/in/chintala-hemanth-842b88217/"}  title={"Chintala hemanth"}>
                             <LinkedIn/>
                         </IconButton>
-                        <IconButton target={"_blank"} href={"https://www.instagram.com/vishalsingh023/"}  title={"vishalsingh023"}>
+                        <IconButton target={"_blank"} href={"https://www.instagram.com/hemanthkumar4105//"}  title={"hemanth kumar"}>
                             <Instagram/>
                         </IconButton>
                         <IconButton target={"_blank"} href={"https://vishal.thetechnician.in/"}  title={"Web Portfolio"}>
